df36f1e9c7e8f3e88a7183b86996f05acd89e059
MCurtisNI <matthew.curtis@ni.com>
2020-11-06 10:28:57 -0600

    Update IBeamformer.cs

--- Source/L2CapstoneProject/IBeamformer.cs
+++ Source/L2CapstoneProject/IBeamformer.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace L2CapstoneProject
 {
-    interface Beamformer
+    interface IBeamformer
     {
+        void Connect();
+
+        void Disconnect();
     }
+
+    interface ISequencedBeamformer: IBeamformer
+    {
+        void LoadSequence();
+
+        void InitiateSequence();
+
+        void AbortSequence();
+    }
+
+    interface IDynamicBeamformer: IBeamformer
+    {
+        void LoadOffset(PhaseAmplitudeOffset offset);
+    }
+
+    interface IDynamicSequencedBeamformer : ISequencedBeamformer, IDynamicBeamformer { }
 }
